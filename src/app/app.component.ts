bfabc5d50e5a443012219bcae82bd94144a69bbb
Naresh Kumar H <nareshkumarh@live.com>
2019-06-28 13:03:30 -0400

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'pipe-demo';
+  currentDate = new Date();
+  products= [ {"id":1,
+  "name":"Dell"}];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { UpperPipe } from './upper.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UpperPipe
   ],
   imports: [
     BrowserModule
